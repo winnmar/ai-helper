cb73ef73e3287b00343b9e0cd4de965ebaf312f5
Mariusz Winnicki <winnickimariusz96@gmail.com>
2025-05-19 01:16:26 +0200

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,5 @@
 import { streamText } from 'ai';
-import { google } from '@ai-sdk/google';
+import { google, GoogleGenerativeAIProviderOptions } from '@ai-sdk/google';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -11,12 +11,19 @@ export async function POST(req: Request) {
     model: google('gemini-2.5-flash-preview-04-17', {
       useSearchGrounding: true,
     }),
-    system: 'You are a board game rules expert, specializing exclusively in the following games: ' +
-    'Everdell, Ark Nova, Wingspan, and Cascadia. You possess comprehensive knowledge of the rules, ' +
-    'mechanics, setup, scoring, and official clarifications for these games. You only answer questions ' +
-    'that are directly related to these four board games. If a question falls outside this scope, respond ' +
-    'politely and inform the user that you can only assist with those games. Always aim to provide accurate, ' +
-    'clear, and concise answers based on the official rulebooks and accepted rulings.',
+    providerOptions: {
+      google: {
+        thinkingConfig: {
+          thinkingBudget: 2048,
+        },
+      } satisfies GoogleGenerativeAIProviderOptions,
+    },
+    system: `You are a board game rules expert, specializing exclusively in the following games: 
+    Everdell, Ark Nova, Wingspan, and Cascadia. You possess comprehensive knowledge of the rules, 
+    mechanics, setup, scoring, and official clarifications for these games. You only answer questions 
+    that are directly related to these four board games. If a question falls outside this scope, respond 
+    politely and inform the user that you can only assist with those games. Always aim to provide accurate, 
+    clear, and concise answers based on the official rulebooks and accepted rulings.`,
     messages,
   });
 
